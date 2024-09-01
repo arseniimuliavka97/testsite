4047502df9485e0957a339f65c50dc2f1890420c
arseniimuliavka97 <142666084+arseniimuliavka97@users.noreply.github.com>
2024-09-01 21:27:10 +0300

    Add files via upload

---
+++ dist/bundle.js
@@ -0,0 +1,449 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 594:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+var map = {
+	"./0fe1f3ad946008de60e978d72ff9d1b5.png": 33,
+	"./11c78caa7961dd1bc87e7fa66b7e2ce8.png": 694,
+	"./30d5f4412151e9c830a421d5eecbe214.png": 654,
+	"./426a101de7585d7d70b1d20cd365e327.png": 118,
+	"./45da23110fc5e62a392e62ee2d50ec9d.png": 248,
+	"./48ff8574284260c656e8c1b228af8eb6.png": 769,
+	"./502d2ce9e955f1db83c82fd4a58e997b.png": 415,
+	"./510b186b476f18748e0024e66742c923.png": 66,
+	"./63610463a85b42cb0ed10fab05d7a15c.png": 7,
+	"./65c8ef551b2f45374394c5fce38b445a.png": 898,
+	"./662359f055a62dae41b505503ce2d6f3.png": 154,
+	"./664b417d9a963291f08ebeda4b9b14bd.png": 399,
+	"./6d9a3c03ecbce284c767edf6c6512f2b.gif": 983,
+	"./7afff20bc4eb69c08ab37e4b96fcb370.png": 970,
+	"./834489e27c8da37ad5f7d208216e5a43.png": 746,
+	"./8da3c65fedfac51189a7a7d13e973d3b.png": 685,
+	"./8dee826c6bca860f32cbee5a78e798e3.png": 571,
+	"./91a445584c87ae3f2b888f7f5019fa8f.png": 363,
+	"./990bc629d1bf2035b68df21cc675c43e.png": 97,
+	"./9d8fa8ce18b29ca52267cd66e6859c78.png": 762,
+	"./Download_on_the_App_Store_Badge_US-UK_RGB_wht_092917.svg": 138,
+	"./a31a2111f4ab1331ffeb8e05c96425af.jpg": 213,
+	"./b132dcdd0a4f789644c12d0bfc070c63.png": 496,
+	"./b40fcc66579d1e7708909ad24c6ea89e.png": 143,
+	"./badge-google-play-and-app-store-button-download-free-png.png": 531,
+	"./bd833bb8554e0b2fa398d56fae7901af.png": 849,
+	"./bd92933e7cfda5193da07c818c4a6406.png": 344,
+	"./bfa06da1bb8b8e40998fd27e466ed151.png": 936,
+	"./c7255edd86875178db0b5d5f3ccb8444.png": 146,
+	"./d1a3a03535a32da920db8d9a9c289df1.jpg": 546,
+	"./draw.png": 930,
+	"./e67b9f04866ff244c3538bfbf0bd37ff.png": 551,
+	"./e7d8d5aea58e363f1200846e0de6bfc5.png": 955,
+	"./f569a0542755ca6af36c6414a322dd47.png": 786,
+	"./fbf1a97a459f0ea9adc19c55a904abb4.jpg": 986,
+	"./speech-bubble.png": 291
+};
+
+
+function webpackContext(req) {
+	var id = webpackContextResolve(req);
+	return __webpack_require__(id);
+}
+function webpackContextResolve(req) {
+	if(!__webpack_require__.o(map, req)) {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	}
+	return map[req];
+}
+webpackContext.keys = function webpackContextKeys() {
+	return Object.keys(map);
+};
+webpackContext.resolve = webpackContextResolve;
+module.exports = webpackContext;
+webpackContext.id = 594;
+
+/***/ }),
+
+/***/ 33:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/0fe1f3ad946008de60e978d72ff9d1b5.png";
+
+/***/ }),
+
+/***/ 694:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/11c78caa7961dd1bc87e7fa66b7e2ce8.png";
+
+/***/ }),
+
+/***/ 654:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/30d5f4412151e9c830a421d5eecbe214.png";
+
+/***/ }),
+
+/***/ 118:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/426a101de7585d7d70b1d20cd365e327.png";
+
+/***/ }),
+
+/***/ 248:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/45da23110fc5e62a392e62ee2d50ec9d.png";
+
+/***/ }),
+
+/***/ 769:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/48ff8574284260c656e8c1b228af8eb6.png";
+
+/***/ }),
+
+/***/ 415:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/502d2ce9e955f1db83c82fd4a58e997b.png";
+
+/***/ }),
+
+/***/ 66:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/510b186b476f18748e0024e66742c923.png";
+
+/***/ }),
+
+/***/ 7:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/63610463a85b42cb0ed10fab05d7a15c.png";
+
+/***/ }),
+
+/***/ 898:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/65c8ef551b2f45374394c5fce38b445a.png";
+
+/***/ }),
+
+/***/ 154:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/662359f055a62dae41b505503ce2d6f3.png";
+
+/***/ }),
+
+/***/ 399:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/664b417d9a963291f08ebeda4b9b14bd.png";
+
+/***/ }),
+
+/***/ 983:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/6d9a3c03ecbce284c767edf6c6512f2b.gif";
+
+/***/ }),
+
+/***/ 970:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/7afff20bc4eb69c08ab37e4b96fcb370.png";
+
+/***/ }),
+
+/***/ 746:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/834489e27c8da37ad5f7d208216e5a43.png";
+
+/***/ }),
+
+/***/ 685:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/8da3c65fedfac51189a7a7d13e973d3b.png";
+
+/***/ }),
+
+/***/ 571:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/8dee826c6bca860f32cbee5a78e798e3.png";
+
+/***/ }),
+
+/***/ 363:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/91a445584c87ae3f2b888f7f5019fa8f.png";
+
+/***/ }),
+
+/***/ 97:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/990bc629d1bf2035b68df21cc675c43e.png";
+
+/***/ }),
+
+/***/ 762:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/9d8fa8ce18b29ca52267cd66e6859c78.png";
+
+/***/ }),
+
+/***/ 138:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/Download_on_the_App_Store_Badge_US-UK_RGB_wht_092917.svg";
+
+/***/ }),
+
+/***/ 213:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/a31a2111f4ab1331ffeb8e05c96425af.jpg";
+
+/***/ }),
+
+/***/ 496:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/b132dcdd0a4f789644c12d0bfc070c63.png";
+
+/***/ }),
+
+/***/ 143:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/b40fcc66579d1e7708909ad24c6ea89e.png";
+
+/***/ }),
+
+/***/ 531:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/badge-google-play-and-app-store-button-download-free-png.png";
+
+/***/ }),
+
+/***/ 849:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/bd833bb8554e0b2fa398d56fae7901af.png";
+
+/***/ }),
+
+/***/ 344:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/bd92933e7cfda5193da07c818c4a6406.png";
+
+/***/ }),
+
+/***/ 936:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/bfa06da1bb8b8e40998fd27e466ed151.png";
+
+/***/ }),
+
+/***/ 146:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/c7255edd86875178db0b5d5f3ccb8444.png";
+
+/***/ }),
+
+/***/ 546:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/d1a3a03535a32da920db8d9a9c289df1.jpg";
+
+/***/ }),
+
+/***/ 930:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/draw.png";
+
+/***/ }),
+
+/***/ 551:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/e67b9f04866ff244c3538bfbf0bd37ff.png";
+
+/***/ }),
+
+/***/ 955:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/e7d8d5aea58e363f1200846e0de6bfc5.png";
+
+/***/ }),
+
+/***/ 786:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/f569a0542755ca6af36c6414a322dd47.png";
+
+/***/ }),
+
+/***/ 986:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/fbf1a97a459f0ea9adc19c55a904abb4.jpg";
+
+/***/ }),
+
+/***/ 291:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "images/speech-bubble.png";
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src;
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) {
+/******/ 					var i = scripts.length - 1;
+/******/ 					while (i > -1 && (!scriptUrl || !/^http(s?):/.test(scriptUrl))) scriptUrl = scripts[i--].src;
+/******/ 				}
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+
+;// CONCATENATED MODULE: ./module.js
+const ANSWER = 54
+;// CONCATENATED MODULE: ./index.js
+
+//import './style.css'
+
+
+console.log( {ANSWER: ANSWER} );
+
+function importAll(r) {
+  return r.keys().map(r);
+}
+  
+  const index_images = importAll(__webpack_require__(594));
+  
+  index_images.forEach((img, index) => {
+    console.log(`Image ${index}:`, img);
+  });
+})();
+
+/******/ })()
+;
